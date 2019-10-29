5ad8260909a00021243c25e134eb14230ec4b42e
Oussama Sarhraoui <oussama.sarhraoui@schibsted.com>
2019-10-29 09:19:21 +0100

    Add podcast entity

--- src/podcast/podcast.js
+++ src/podcast/podcast.js
@@ -1,7 +1,53 @@
 // eslint-disable-next-line no-empty-pattern
-export default function buildMakePodCast ({ }) {
+export default function buildMakepodcast ({ uuid }) {
   // eslint-disable-next-line no-empty-pattern
-  return function MakePodCast ({}) {
-    return Object.freeze({})
+  return function makePodcast ({
+    url,
+    podcastId = uuid(),
+    episodes = [],
+    owner,
+    title
+  } = {}) {
+    if (!isValidUrl(url)) {
+      throw new Error(`'${url}' is not a valid url.`)
+    }
+    if (!podcastId) {
+      throw new Error(`'${podcastId}' is not a valid podcast Id.`)
+    }
+
+    if (!episodes || !Array.isArray(episodes)) {
+      throw new Error(`'${episodes}' is not a valid episodes list.`)
+    }
+
+    if (owner && typeof owner !== 'string') {
+      throw new Error(`owner: '${owner}' must be of type string.`)
+    }
+
+    if (title && typeof title !== 'string') {
+      throw new Error(`title: '${title}' must be of type string.`)
+    }
+    /*
+    const validEpisodes = []
+    epispdes.forEach(episode => {
+      validEpisodes.push(makeEpisode(episode))
+    })
+    */
+
+    function isValidUrl (str) {
+      if (str && typeof str === 'string') {
+        return true
+      }
+      return false
+    }
+
+    return Object.freeze({
+      getUrl: () => url,
+      getPodcastId: () => podcastId,
+      getOwner: () => owner || '',
+      getTitle: () => title || ''
+      /*,
+      getEpisodes: () => validEpisodes
+       */
+    })
   }
 }
--- src/podcast/podcast.spec.js
+++ src/podcast/podcast.spec.js
@@ -1,8 +1,131 @@
-import makePodCast from './'
+import { makeFakePodcast } from '../../__test__/data/podcast'
+import makePodcast from './'
 
-describe('podcast', () => {
-  it('Must have an url', () => {
-    const podcast = { url: null }
-    expect(() => makePodCast(podcast).toThrow('podcast must contain an url'))
+describe('Podcast', () => {
+  it('must have an url', () => {
+    var fakePodcast
+    const urls = [
+      null,
+      undefined,
+      ''
+    ]
+    urls.forEach(url => {
+      fakePodcast = makeFakePodcast({ url: url })
+      expect(() => makePodcast(fakePodcast)).toThrow(`'${url}' is not a valid url.`)
+    })
   })
+
+  it('must have a valid PodcastId', () => {
+    var fakePodcast
+    const podcastIds = [
+      null,
+      ''
+    ]
+    podcastIds.forEach(podcastId => {
+      fakePodcast = makeFakePodcast({ podcastId: podcastId })
+      expect(() => makePodcast(fakePodcast)).toThrow(`'${podcastId}' is not a valid podcast Id.`)
+    })
+  })
+
+  it('must have an episodes list', () => {
+    var fakePodcast
+    const nonValidEpisodes = [
+      null,
+      '',
+      'qddqsdq',
+      {}
+    ]
+    nonValidEpisodes.forEach(episodes => {
+      fakePodcast = makeFakePodcast({ episodes: episodes })
+      expect(() => makePodcast(fakePodcast)).toThrow(`'${episodes}' is not a valid episodes list.`)
+    })
+    const episodes = []
+    // Empty list of Episodes
+    fakePodcast = makeFakePodcast({ episodes: episodes })
+    expect(() => makePodcast(fakePodcast)).not.toThrow(`'${episodes}' is not a valid episodes list.`)
+    // Non empty List of episodes
+    fakePodcast = makeFakePodcast()
+    expect(() => makePodcast(fakePodcast)).not.toThrow(`'${episodes}' is not a valid episodes list.`)
+  })
+
+  it('can have an title', () => {
+    var fakePodcast
+    const nullTitles = [
+      null,
+      undefined,
+      ''
+    ]
+
+    nullTitles.forEach(title => {
+      fakePodcast = makeFakePodcast({ title: title })
+      expect(() => makePodcast(fakePodcast)).not.toThrow()
+      const podcast = makePodcast(fakePodcast)
+      expect(podcast.getTitle()).toEqual('')
+    })
+
+    const nonValidtTitles = [
+      [],
+      {},
+      4.5
+    ]
+
+    nonValidtTitles.forEach(title => {
+      fakePodcast = makeFakePodcast({ title: title })
+      expect(() => makePodcast(fakePodcast)).toThrow(`title: '${title}' must be of type string.`)
+    })
+
+    const validTitles = [
+      'My cool Title'
+    ]
+
+    validTitles.forEach(title => {
+      fakePodcast = makeFakePodcast({ title: title })
+      expect(() => makePodcast(fakePodcast)).not.toThrow()
+      const podcast = makePodcast(fakePodcast)
+      expect(podcast.getTitle()).toEqual(title)
+    })
+  })
+
+  it('can have an owner', () => {
+    var fakePodcast
+    const nullOwners = [
+      null,
+      undefined,
+      ''
+    ]
+
+    nullOwners.forEach(owner => {
+      fakePodcast = makeFakePodcast({ owner: owner })
+      expect(() => makePodcast(fakePodcast)).not.toThrow()
+      const podcast = makePodcast(fakePodcast)
+      expect(podcast.getOwner()).toEqual('')
+    })
+
+    const nonValidOwners = [
+      [],
+      {},
+      4.5
+    ]
+
+    nonValidOwners.forEach(owner => {
+      fakePodcast = makeFakePodcast({ owner: owner })
+      expect(() => makePodcast(fakePodcast)).toThrow(`owner: '${owner}' must be of type string.`)
+    })
+
+    const validOwners = [
+      'oussama@gmail.com'
+    ]
+
+    validOwners.forEach(owner => {
+      fakePodcast = makeFakePodcast({ owner: owner })
+      expect(() => makePodcast(fakePodcast)).not.toThrow()
+      const podcast = makePodcast(fakePodcast)
+      expect(podcast.getOwner()).toEqual(owner)
+    })
+  })
+
+  xit('can have a pubDate', () => {})
+  xit('can have description', () => {})
+  xit('sanitize its title', () => {})
+  xit('sanitize its description', () => {})
 })

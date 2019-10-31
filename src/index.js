84efca8d9fae1ba7bf3f79220d37f37d976df70d
Oussama Sarhraoui <oussama.sarhraoui@schibsted.com>
2019-10-31 21:18:17 +0100

    Add api endpoints and logs to express app

--- src/index.js
+++ src/index.js
@@ -1,7 +1,23 @@
 import express from 'express'
-import bodyParser from 'body-parser'
+import './conf'
+import podcastController from './controllers'
+import morgan from 'morgan'
 
 const app = express()
-app.use(bodyParser.json())
 
+app.use(morgan('combined'))
+
+app.get(/\/episodes\/?/, async (req, res) => {
+  const result = await podcastController.getEpisodes(req)
+  res.set(result.headers)
+    .type('json')
+    .status(result.statusCode)
+    .send(result.body)
+})
+
+app.use(function (req, res) {
+  res.send(404)
+})
+
+app.listen(8080, () => console.log('Example app listening on port 8080!'))
 export default app
